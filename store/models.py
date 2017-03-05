ddf1edd2a92b1f024adcd96e512bbc8f4d3401ce
Cromwell Cutaran <cromwell@Cromwells-MacBook-Pro.local>
2017-03-05 00:31:42 -0800

    Initial models/database setup

--- store/models.py
+++ store/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
+# represents a typical product in OFS
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    # arguments of model type means
+    #  an item can be up to $99.99
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    # using CharField for now
+    #  use FilePathField instead ?
+    image_path = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
 
