bfb8eed373c8d5938b7459740e8bc4361008f7b4
isabelchan <chanisabelbel@yahoo.com>
2017-03-22 22:21:49 -0700

    Add files via upload

--- store/views.py
+++ store/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("OFS Test")
+    #image = SM_produce.objects.get(id=1)
+    #img = image.image_path
+    #return render(request, 'lesson': img)
+    return render(request, 'store/index0.html')
+
