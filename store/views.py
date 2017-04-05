5a13c31a9950a65f376555a618912bded6c74e30
Cromwell Cutaran <cromwell@Cromwells-MacBook-Pro-220.local>
2017-04-04 20:31:35 -0700

    Routing to product pages

--- store/views.py
+++ store/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'store/index0.html')
+
+def products(request):
+    location = 'Santa Clara' if request.path.split('_')[1] == 'sc' \
+        else 'San Mateo'
+    return HttpResponse("Products for " + location + " location")
+
+def tracking(request):
+    return HttpResponse("Tracking")
+    
