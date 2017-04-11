f7c79bd314ebfd6213351a95ba2f1c7661e05dec
Kenton <huangkenton@gmail.com>
2017-04-10 23:49:31 -0700

    order database testing

--- orders/views.py
+++ orders/views.py
@@ -14,19 +14,44 @@ from store.models import SC_produce, SM_produce
 #    return render(request, 'orders/orders.html')
 
 def orders(request):
+    #test items and test quantities
+    itemid1 = 1
+    itemid2 = 2
+    itemid3 = 3
+    quan1 = 4
+    quan2 = 1
+    quan3 = 2
+    #request.session from cart
+    productsSC = SC_produce.objects.all()
+    items = [itemid1, itemid2, itemid3]
+    itemquery = []
+    quantity = [quan1, quan2, quan3]
+    itemprices = []
+    for item in items:
+        itemquery.extend(productsSC.filter(id=item))
+    for item, quan in zip(itemquery,quantity):
+        itemprice = item.price
+        itemprices.append(itemprice * quan)
+    print(itemprices)
+    combined = zip(itemquery, quantity, itemprices)
+    total = 0
+    for prices in itemprices:
+        total += prices
+
+    print(total)
+    li_result = list(combined)
+
+    print(li_result)
+
+
+
+
     form = OrderForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
         print(instance.order_id)
         request.session['order_id'] = instance.order_id
-
-        itemid1 = 1
-        itemid2 = 2
-        itemid3 = 3
-        quan1 = 4
-        quan2 = 1
-        quan3 = 2
 
         OrderItem1 = OrderItems.objects.create(order_id=instance.order_id, item_id=itemid1, quantity=quan1)
         OrderItem2 = OrderItems.objects.create(order_id=instance.order_id, item_id=itemid2, quantity=quan2)
@@ -48,4 +73,4 @@ def orders(request):
 
 
 
-    return render(request, "orders/orders.html", {'myform': form})
+    return render(request, "orders/orders.html", {'myform': form, 'li_result':li_result, "total": total})
