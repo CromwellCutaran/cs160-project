849f1002fd56a6f443d9d6304b7d1a78b9abce2f
Cromwell Cutaran <CromwellCutaran@gmail.com>
2017-04-11 00:39:34 -0700

    Added AJAX for product page

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from .models import SM_produce, SC_produce
-
+import pdb
+import ast
 
 def index(request):
     return render(request, 'store/index0.html')
@@ -11,10 +13,18 @@ def products(request):
         else 'SM_produceStore.html'
     db_products = SC_produce.objects.all() if request.path.split('_')[1] == 'sc' \
         else SM_produce.objects.all()
+    db_products_json = ast.literal_eval(serializers.serialize('json', db_products))
     context = {
-        'db_products' : db_products,
+        'db_products' : db_products
     }
+    pdb.set_trace()
     return render(request, "store/" + location, context)
 
 def tracking(request):
     return HttpResponse("Tracking")
+
+def update_cart(request):
+    if not request.is_ajax() or not request.method=='POST':
+        return HttpResponseNotAllowed(['POST'])
+
+    request.session['m']
