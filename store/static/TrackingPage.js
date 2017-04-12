f1952324ed200d095c49adb85127acfc6246b2f8
gharkamal <Grewal@Gs-MacBook-Pro.local>
2017-04-12 00:08:33 -0700

    tracking page error

--- store/static/TrackingPage.js
+++ store/static/TrackingPage.js
@@ -1,7 +1,16 @@
+
+
+//index page variables
 var trackingNumber = document.getElementById("trackingNumberInput")
 var clickButton  = document.getElementById("submitButton")
 
+//console.log("in tracking")
+//name.innerText = "Harkamal";
+
+var data = null;
+
 clickButton.addEventListener('click', sendTracking)
+
 
 function sendTracking(e)
 {
@@ -9,21 +18,23 @@ function sendTracking(e)
 	if(trackingNumber.value != "")
 	{
 		var trackNumber = trackingNumber.value;
-	console.log("in sendTracking " + trackingNumber.value);
-    //window.location.href = "track";
+    
 	console.log("sending trackNumber to server....");
    $.ajax({
-        url : "./post_tracking", // the endpoint
+        url : "./track", // the endpoint
         type : "GET", // http method
-        data : { tNumber : trackNumber,
+        data : { 'tNumber' : trackNumber,
             'csrfmiddlewaretoken': '{{ csrf_token }}'
          }, // data sent with the post request
 
         // handle a successful response
         success : function(json) {
-            $('#trackingNumberInput').val(''); // remove the value from the input
-            console.log("tracking number sent back by server",json); // log the returned json to the console
+            //$('#trackingNumberInput').val(''); // remove the value from the input
+            console.log("tracking number sent back by server", json); // log the returned json to the console
+            //setTrackingPageValues(trackNumber);
             console.log("success"); // another sanity check
+             //window.location.href = "track";
+        
         },
 
         // handle a non-successful response
@@ -33,6 +44,7 @@ function sendTracking(e)
         }
         
     });
+    //getData(trackNumber);
 
 	}
 	else 
@@ -41,3 +53,36 @@ function sendTracking(e)
 	}
 	
 }
+
+
+//setting tracking page values 
+function setTrackingPageValues(json)
+{
+    console.log("setting values for tracking page")
+    var data = JSON.parse(json);
+    $("#name").val(data['first_name'] + ' ' + data['last_name']);
+    $("#data-purchased").val(data['timestamp']);
+    $("#store-location").val(data['store_location']);
+    $("#shippingAddr").val( data['address'] + ' ' + data['city'] + ' ' + data['state'] + ' ' + data['zipcode']);
+    $("#conf-email").val(data['email']);
+    
+    $("#amt-paid").val(data['price']);
+    
+    console.log(" expected time: ",$("#exp-arrival").val());
+
+   /* name.innerText = data['first_name'] + ' ' + data['last_name'];
+    date_purchased.innerText = data['timestamp']
+    store_location.innerText = date['store_location']
+    shippingAddr.innerText = data['address'] + ' ' + data['city'] + ' ' + data['state'] + ' ' + data['zipcode']
+    conf_email.innerText = data['email']
+    //exp_arrival.data = data['']
+    amt_paid.innerText = data['price']
+*/
+    window.location.href = "track";
+
+
+}
+
+
+
+
--- store/views.py
+++ store/views.py
@@ -7,17 +7,43 @@ from django.views.decorators.http import require_GET
 from orders.models import OrderItems
 from orders.models import Order
 import pdb
+import simplejson as json
+from django.core import serializers
+import ast
+import json as js
 
 
+
+
+jsonTemp = None
 
 
 
 def index(request):
     return render(request, 'store/index0.html')
 
-
+@require_GET
+@csrf_exempt
 def track(request):
-    return render(request, 'store/trackingPage.html')
+ #  tracking_number = request.GET.get('tNumber')
+    ''' 
+    order = Order.objects.get(order_id=tracking_number)
+    data = {}
+    data['order_id'] = order.order_id
+    data['first_name'] = order.first_name
+    data['last_name'] = order.last_name
+    data['address'] = order.address
+    data['city'] = order.city
+    data['state'] = order.state
+    data['store_location'] = order.location
+    data['price'] = order.price_total
+    data['timestamp'] = str(order.timestamp)
+    data['zipcode'] = order.zipcode
+    data['state'] = order.state
+    data['email'] = order.email
+    '''
+    if request.method == 'GET':
+        return render(request, 'store/trackingPage.html', {"fname": request.GET.get('tNumber')})
 
 @require_GET
 @csrf_exempt
@@ -27,9 +53,26 @@ def post_tracking(request):
 
        # return HttpResponse("SERVER: "+ tracking_number)
         try:
+            #order = list(Order.objects.get(order_id=tracking_number))
+            #db_order_json = ast.literal_eval(serializers.serialize('json', order))
             order = Order.objects.get(order_id=tracking_number)
+            data = {}
+            data['order_id'] = order.order_id
+            data['first_name'] = order.first_name
+            data['last_name'] = order.last_name
+            data['address'] = order.address
+            data['city'] = order.city
+            data['state'] = order.state
+            data['store_locatison'] = order.location
+            data['price'] = order.price_total
+            data['timestamp'] = str(order.timestamp)
+            data['zipcode'] = order.zipcode
+            data['state'] = order.state
+            data['email'] = order.email
+            json_data = json.dumps(data)
+            jsonTemp = json_data
            # pdb.set_trace()
-            return HttpResponse(order.first_name)
+            return HttpResponse(json_data)
         except Order.DoesNotExist:
             raise Http404("Order does not exist")
 
