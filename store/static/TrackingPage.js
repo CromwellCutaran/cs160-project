7f09e12b23dda097c522d8c3a89842288d624fa1
gharkamal <Grewal@Gs-MacBook-Pro.local>
2017-04-20 15:30:46 -0700

    checking

--- store/static/TrackingPage.js
+++ store/static/TrackingPage.js
@@ -52,6 +52,12 @@ function sendTracking(e)
 	
 }
 
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: iconBase + 'parking_lot_maps.png'
+  });
 
 
 
