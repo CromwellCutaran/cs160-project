416dbeb592f4e13aa62303f2dc145167c0b970ff
gharkamal <Grewal@Gs-MacBook-Pro.local>
2017-04-24 21:03:31 -0700

    dynamically render map

--- store/static/TrackingPage.js
+++ store/static/TrackingPage.js
@@ -8,9 +8,10 @@ var clickButton  = document.getElementById("submitButton")
 //name.innerText = "Harkamal";
 
 var data = null;
-
+if(trackingNumber)
+{
 clickButton.addEventListener('click', sendTracking)
-
+}
 
 function sendTracking(e)
 {
@@ -29,7 +30,7 @@ function sendTracking(e)
         // handle a successful response
         success : function(json) {
             //$('#trackingNumberInput').val(''); // remove the value from the input
-            console.log("tracking number sent back by server", json); // log the returned json to the console
+          //  console.log("tracking number sent back by server", json); // log the returned json to the console
           //  setTrackingPageValues(json);
           window.location.href = 'track'; //ocnce successfully data is sent then track is called in URL to Views
             console.log("success"); // another sanity check
@@ -51,8 +52,65 @@ function sendTracking(e)
 	}
 	
 }
+//----------------------------------------------------------------------------
+ //----------------------------------------------------------------------------
+ //----------------------------------------------------------------------------
+ 
 
 
+var addressLoc =  null;
+var storeLoc = null;
 
+
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 37.56,  lng: -122.32} //santa clara 
+  });
+  directionsDisplay.setMap(map);
+  //console.log("in initMap");
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+    calMidPoint(directionsService, directionsDisplay);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+   // console.log("in calculateAndDisplayRoute")
+  directionsService.route({
+    origin: $('#storeLoc').val() ,
+    destination: $('#address').val(),
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+
+function calMidPoint()
+{
+    console.log("calculating calMidPoint");
+    var context = {
+        origin: $('#storeLoc').val(),
+        destination: $('#address').val(),
+        travelMode: 'DRIVING'
+    };
+
+}
+
+function sendLoc(data)
+{
+   addressLoc =  $('#address').val();
+   storeloc =  $('#storeLoc').val();
+    console.log(addressLoc);
+    console.log(storeloc);
+    //initMap()
+}
+
+document.addEventListener('DOMContentLoaded', sendLoc);
 
 
